faf03dd32ca6d039c86aa4f437471dc971df5c42
day0hacker <thebestcherepasha@yandex.ru>
2018-01-10 17:16:49 +0300

    deleted comment

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ namespace Externalio
 {
     class Program
     {
-        // commment
         static void Main(string[] args)
         {
             Console.Title = $"Externalio - Build ({ Extensions.AssemblyCreationDate() })";
